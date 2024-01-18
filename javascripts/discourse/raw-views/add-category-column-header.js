545aa7e11fa5c9653ea8bcef75df2564bbcc9a81
communiteq <support@discoursehosting.com>
2024-01-18 11:10:57 +0100

    Rewrite without overriding templates (#6)
    This theme component was broken (bulk select didn't work anymore)
    When trying to fix it we saw that there are enough raw plugin outlets to be able to add the column without overriding hbs templates, so the theme component was completely reworked to use raw plugin outlets.

---
+++ javascripts/discourse/raw-views/add-category-column-header.js
@@ -0,0 +1,10 @@
+import EmberObject from "@ember/object";
+
+export default class extends EmberObject {
+    get hideCategory() {
+        return this.parent.context.hideCategory;
+    }
+    get sortable() {
+        return this.parent.context.sortable;
+    }
+}
