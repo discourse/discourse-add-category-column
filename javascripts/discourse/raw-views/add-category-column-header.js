97478889a6781e9d8eecc20610fa5976b02b1ec9
tshenry <taylor@discourse.org>
2024-02-20 15:47:05 -0800

    FIX: do not attempt to add column to mobile

--- javascripts/discourse/raw-views/add-category-column-header.js
+++ javascripts/discourse/raw-views/add-category-column-header.js
@@ -1,10 +1,10 @@
 import EmberObject from "@ember/object";
 
 export default class extends EmberObject {
-    get hideCategory() {
-        return this.parent.context.hideCategory;
-    }
-    get sortable() {
-        return this.parent.context.sortable;
-    }
+  get hideCategory() {
+    return this.parent.context.hideCategory;
+  }
+  get sortable() {
+    return this.parent.context.sortable;
+  }
 }
