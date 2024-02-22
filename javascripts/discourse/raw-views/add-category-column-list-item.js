98f6c07e6605b5c6ea23f40a24a26d8effbf4f2a
tshenry <taylor@discourse.org>
2024-02-21 20:16:07 -0800

    FIX: do not attempt to add column to mobile (#7)

--- javascripts/discourse/raw-views/add-category-column-list-item.js
+++ javascripts/discourse/raw-views/add-category-column-list-item.js
@@ -1,7 +1,10 @@
 import EmberObject from "@ember/object";
 
 export default class extends EmberObject {
-    get hideCategory() {
-        return this.parent.context.hideCategory;
-    }
+  get hideCategory() {
+    return this.parent.context.hideCategory;
+  }
+  get mobileView() {
+    return this.site.mobileView;
+  }
 }
